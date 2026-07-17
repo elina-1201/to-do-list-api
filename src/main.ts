d0aec177cf137ab52823dbe9b0ad45c260a4f87c
elina-1201 <elina.mursalimova16@gmail.com>
2026-07-17 02:16:47 +0200

    Setup Swagger and edit README

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ################## Swagger setup #####################################
+  const config = new DocumentBuilder()
+    .setTitle('Swagger')
+    .setVersion('1.0')
+    .addTag('swagger')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  // ################## Swagger setup #####################################
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
